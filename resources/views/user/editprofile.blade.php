b28684064a52bfe163b914bb12f45ec399bb6042
Rauan <r.adilgazin@gmail.com>
2023-11-26 02:52:14 +0600

    inital

--- resources/views/user/editprofile.blade.php
+++ resources/views/user/editprofile.blade.php
@@ -21,10 +21,35 @@
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <input name="name"  type="text" id="form3Example1c" class="form-control" value="{{ $user->name }}"/>
-                      <label class="form-label" for="form3Example1c">Your Name</label>
+                      <input name="fname"  type="text" id="form3Example1c" class="form-control" value="{{ $user->fname }}"/>
+                      <label class="form-label" for="form3Example1c">Your First Name</label>
                     </div>
                   </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input name="lname"  type="text" id="form3Example1c" class="form-control" value="{{ $user->lname }}"/>
+                      <label class="form-label" for="form3Example1c">Your Last Name</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                       <input name="birthday" type="date" id="form3Example1c" class="form-control" value="{{ old('birthday', $user->birthday ?? '') }}">
+                       <label for="birthday">Birth Date</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                       <input name="phone_number" type="tel" id="phone" class="form-control" pattern="+[7]{1}([0-9]){3}-[0-9]{3}-[0-9]{2}-[0-9]{2}" placeholder="+7(700)-000-00-00" value="{{ $user->phone_number}}">
+                       <label for="phone">Phone number</label>
+                    </div>
+                  </div>
+
 
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Ссылка на форму входа в учетную запись пользователя
 Route::get('/login', 'AuthController@showLoginForm');
-Route::get('/register', 'UserController@showRegistrationForm')->name('register');
 Route::post('/log', 'AuthController@login')->name('login');
+// Ссылка на форму регистрации в учетную запись пользователя
+Route::get('/register', 'UserController@showRegistrationForm')->name('register');
 Route::post('/register', 'UserController@create');
 
+// Ссылки на профиль пользователя
 Route::get('/profile/{user}','ProfileController@show')->name('profile');
+// Ссылки на изменение и редактиврование данных пользователя профиль пользователя
 Route::get('/profile/{user}/edit','ProfileController@edit')->name('profile.edit');
 Route::patch('/profile/{user}/update','ProfileController@update')->name('profile.update');
+
+Route::get('/messages','MessageController@index');
 
 // Route::post('/logout', 'LoginController@logout')->name('logout');
 // Route::get('/profile', 'ProfileController@index')->middleware('auth');
